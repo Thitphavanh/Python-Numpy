8c1b00f02cb46c99ae2f7ce84405ecfcaa11fd9b
Hery <thitphavanh.psv@gmail.com>
2022-08-04 06:40:59 +0700

    update

---
+++ ep17_slice_array_2d_numpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(a)
+# print(a[:, :])
+# print(a[1:, 1:])
+# print(a[2:, 2:])
+# print(a[2:, 1:])
+# print(a[:, 2:])
+# print(a[1:, :])
+# print(a[:2, :])
+# print(a[:2, 2:])
+# print(a[1:2, 1:2])
+# print(a[::2, :])
+# print(a[1:2, :])
+# print(a[::,::2])
+print(a[::,1:2])
